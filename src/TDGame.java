0ae4271acd35e858ccc500d5d5d7f97c0fc567d3
NickCrawford <ncrawfo7@kent.edu>
2015-10-09 23:28:49 -0400

    Add ClassPath file. Create JFrame in main class

--- src/TDGame.java
+++ src/TDGame.java
@@ -1,9 +1,13 @@
+import javax.swing.JFrame;
+
+
 
 public class TDGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JFrame frame = new JFrame("TDGame");
+		frame.setSize(800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
