4638edb0025155f421f4b7b8833f8f812c211583
NickCrawford <ncrawfo7@kent.edu>
2015-10-09 23:34:56 -0400

    Set up game loop

--- src/TDGame.java
+++ src/TDGame.java
@@ -1,13 +1,32 @@
 import javax.swing.JFrame;
 
-
-
 public class TDGame {
 
+	public static final long DELAY = 100; //Miliseconds to wait between game updates
+
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("TDGame");
+		JFrame frame = new JFrame("TDGame"); //Create a JFrame Object and set its options
 		frame.setSize(800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		
+//		GameComponent game = new GameComponent(curTime);
+		
+//		frame.addMouseListener(game);
+//		frame.add(game);
+		frame.setVisible(true);
+		
+		while(true) {
+			
+			frame.repaint();
+			try {
+				Thread.sleep(DELAY);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
 	}
 
 }
