3103579b6b8c424ae367fdb50a20f9fbca90655c
NickCrawford <ncrawfo7@kent.edu>
2015-10-10 03:28:35 -0400

    Render test tiles from array

--- src/GameComponent.java
+++ src/GameComponent.java
@@ -23,6 +23,7 @@ public class GameComponent extends JComponent implements MouseListener {
 		this.curTime = curTime;
 		
 		boardMap = TEST_MAP;
+
 	}
 	
 	public void paintComponent(Graphics g) {
@@ -36,10 +37,35 @@ public class GameComponent extends JComponent implements MouseListener {
 		for(int j = 0; j < this.getHeight(); j += GRID_SIZE) {//rows
 			g2.drawLine(0, j, this.getWidth(), j);
 		}
+		
+		//Iterate through tile array
+		for(int x=0; x < boardMap.length; x++) {
+			for(int y=0; y < boardMap[x].length; y++) {
+				switch(boardMap[x][y]) {
+				case 0: g2.setColor(Color.WHITE);
+					break;
+				case 1: g2.setColor(Color.GREEN);
+					break;
+				case 2: g2.setColor(Color.RED);
+					break;
+				case 3: g2.setColor(Color.BLACK);
+					break;
+				default: g2.setColor(Color.GRAY);
+					break;
+				}
+				g2.drawRect(y*GRID_SIZE, x*GRID_SIZE, GRID_SIZE, GRID_SIZE);
+			}
+		}
+
+
 	}
 
 	public void update(long nextCurTime) {
 		long elapsedTime = nextCurTime - curTime;
+		
+		for(GameObject obj: gameObjects) {
+			obj.update(elapsedTime);
+		}
 		
 		curTime = nextCurTime;
 	}
@@ -76,10 +102,10 @@ public class GameComponent extends JComponent implements MouseListener {
 
 	
 	public static final int[][] TEST_MAP = {
+		{1,0,1,1,1},
 		{1,1,0,1,1},
+		{1,1,1,0,1},
 		{1,1,0,1,1},
-		{1,1,0,1,1},
-		{1,1,0,1,1},
-		{1,1,0,1,1}
+		{1,0,1,1,1}
 	};
 }
