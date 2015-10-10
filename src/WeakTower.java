71d07438e9515566073b2fd6fdfe6f18ef2048f2
NickCrawford <ncrawfo7@kent.edu>
2015-10-10 12:37:51 -0400

    Actually adds weaktwoer class

---
+++ src/WeakTower.java
@@ -0,0 +1,29 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+
+public class WeakTower extends Tower {
+	
+	public WeakTower(int x, int y) {
+		super(x, y);
+		fileName = "WeakTower.png";
+		rows = 1;
+		columns = 1;
+		size = 64;
+		sprite = super.initSprite(fileName, rows, columns, size);
+		
+	}
+
+	@Override
+	public void update(long elapsedTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(Graphics2D g2) {
+		g2.setColor(Color.PINK);
+		g2.fillRect(x,y,size,size);
+	}
+
+}
