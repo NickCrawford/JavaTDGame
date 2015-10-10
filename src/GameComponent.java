95eec7dcfb1429d00e2badefd1509fc166515617
NickCrawford <ncrawfo7@kent.edu>
2015-10-10 03:43:41 -0400

    Adds implementation for slicing and rendering background tiles

--- src/GameComponent.java
+++ src/GameComponent.java
@@ -1,9 +1,12 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.JComponent;
@@ -18,11 +21,31 @@ public class GameComponent extends JComponent implements MouseListener {
 	private ArrayList<GameObject> gameObjects; //Arraylist of GameObjects in the foreground
 	
 	private int[][] boardMap; //2D array of ints that represent board tiles.
+	private ArrayList<Image> tiles = new ArrayList<Image>();
 	
 	public GameComponent(long curTime) {
 		this.curTime = curTime;
 		
 		boardMap = TEST_MAP;
+
+		initTiles();
+	}
+	
+	private void initTiles() {
+		BufferedImageLoader loader = new BufferedImageLoader();
+		BufferedImage spriteSheet = null;
+		try {
+			spriteSheet = loader.loadImage("TileSet.jpeg");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		SpriteSheet ss = new SpriteSheet(spriteSheet);
+
+		for(int i = 0; i < 128; i += GRID_SIZE) {
+			for(int j = 0; j < 128; j += GRID_SIZE) {
+				tiles.add(ss.grabSprite(j,i,GRID_SIZE,GRID_SIZE));
+			}
+		}
 
 	}
 	
@@ -38,22 +61,23 @@ public class GameComponent extends JComponent implements MouseListener {
 			g2.drawLine(0, j, this.getWidth(), j);
 		}
 		
+		int imageIndex = 0;
 		//Iterate through tile array
 		for(int x=0; x < boardMap.length; x++) {
 			for(int y=0; y < boardMap[x].length; y++) {
 				switch(boardMap[x][y]) {
-				case 0: g2.setColor(Color.WHITE);
+				case 0: imageIndex = 0;
 					break;
-				case 1: g2.setColor(Color.GREEN);
+				case 1: imageIndex = 1;
 					break;
-				case 2: g2.setColor(Color.RED);
+				case 2: imageIndex = 2;
 					break;
-				case 3: g2.setColor(Color.BLACK);
+				case 3: imageIndex = 3;
 					break;
-				default: g2.setColor(Color.GRAY);
+				default: imageIndex = -1;
 					break;
 				}
-				g2.drawRect(y*GRID_SIZE, x*GRID_SIZE, GRID_SIZE, GRID_SIZE);
+				g2.drawImage(tiles.get(imageIndex), y*GRID_SIZE, x*GRID_SIZE, null);
 			}
 		}
 
