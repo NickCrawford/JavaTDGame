6473b784f824b2db8503b14422a085efeb7ca7a5
Wolfdragoon31 <pjleyden.mist31@gmail.com>
2015-10-10 01:54:51 -0400

    Added basic units class

---
+++ src/Units.java
@@ -0,0 +1,32 @@
+
+public abstract class Units 
+{
+
+	int xPos = 0;
+	
+	int yPos = 0;
+	
+	int speed = 0;
+	
+	public int getXPos()
+	{
+		return xPos;
+	}
+	
+	public int getYPos()
+	{
+		return yPos;
+	}
+	
+	public int getSpeed()
+	{
+		return speed;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
